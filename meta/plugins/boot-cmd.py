32209066be8cc6ac56eb8e188aa77d1aca926c98
Keyboard Slayer <dalcjor@libmail.eu>
2022-06-27 17:39:03 +0200

    OSDK plugins: added a field for custom bootloader

--- meta/plugins/boot-cmd.py
+++ meta/plugins/boot-cmd.py
@@ -2,30 +2,40 @@
 # -*- coding: utf-8 -*-
 
 import os
+from json import dump as json_dump
 from osdk import build, utils
 from shutil import copy
 
-LOADER = "limine"
+_DEFAULT_LOADER = "limine"
 
 def bootCmd(opts: dict, args: list[str]):
     sysroot = utils.mkdirP(".build/sysroot")
     efi = utils.mkdirP(".build/sysroot/EFI/BOOT")
     boot = utils.mkdirP(".build/sysroot/boot")
+    loader =  _DEFAULT_LOADER if "loader" not in opts else opts["loader"]
 
-    ovmf = utils.downloadFile(
-        "https://retrage.github.io/edk2-nightly/bin/DEBUGX64_OVMF.fd")
-
-    navy = build.buildOne("kernel-x86_64", "kernel")
-    copy(navy, os.path.join(boot, "kernel.elf"))
-
-    match LOADER:
+    match loader:
         case "limine":
             efi_exe = utils.downloadFile("https://github.com/limine-bootloader/limine/raw/v3.8.2-binary/BOOTX64.EFI")
             copy(efi_exe, os.path.join(efi, "BOOTX64.EFI"))
             copy("src/limine/limine.cfg", os.path.join(boot, "limine.cfg"))
         case _:
-            print(f"{utils.Colors.RED} Unknown loader {LOADER} {utils.Colors.RESET}")
+            print(f"{utils.Colors.RED}Unknown loader {loader} {utils.Colors.RESET}")
             exit(1)
+
+    ovmf = utils.downloadFile(
+        "https://retrage.github.io/edk2-nightly/bin/DEBUGX64_OVMF.fd")
+
+    manifest = utils.loadJson("./meta/targets/kernel-x86_64.json")
+    manifest["props"]["loader"] = loader
+
+    with open("./meta/targets/current_build.json", "w") as f:
+        json_dump(manifest, f)
+
+    navy = build.buildOne("current_build", "kernel")
+    copy(navy, os.path.join(boot, "kernel.elf"))
+
+    os.remove("./meta/targets/current_build.json")
 
     qemuCmd = [
         "qemu-system-x86_64",
@@ -43,6 +53,6 @@ def bootCmd(opts: dict, args: list[str]):
 
 __plugin__ = {
     "name": "boot",
-    "desc": "Boot Navy !",
+    "desc": "Boot Navy ! (use --loader=value to change the bootloader)",
     "func": bootCmd
 }
