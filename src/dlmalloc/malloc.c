3775eb3d716fd63d200e630edd24d516eef70d4a
Keyboard Slayer <dalcjor@libmail.eu>
2022-06-28 16:45:50 +0200

    dlmalloc: fix missing defines

--- src/dlmalloc/malloc.c
+++ src/dlmalloc/malloc.c
@@ -4,6 +4,22 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-function"
 
+#define USE_LOCKS 1
+#define USE_SPIN_LOCKS 1
+#define HAVE_MORECORE 1
+#define MORECORE __sysdep_morecore
+#define MORECORE_CANNOT_TRIM
+#define HAVE_MMAP 0
+#define HAVE_MREMAP 0
+#define MMAP_CLEARS 0
+#define LACKS_UNISTD_H
+#define LACKS_SYS_TYPES_H
+#define LACKS_SYS_PARAM_H
+#define LACKS_SYS_MMAN_H
+#define LACKS_STRINGS_H
+#define LACKS_SCHED_H
+#define LACKS_TIME_H
+#define MALLOC_FAILURE_ACTION
 
 /*
   This is a version (aka dlmalloc) of malloc/free/realloc written by
