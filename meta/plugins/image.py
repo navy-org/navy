95522550ba2c59bb437ff007eda09639b8ef954c
Keyboard Slayer <40356204+keyboard-slayer@users.noreply.github.com>
2024-09-18 18:50:02 +0200

    feat: Build and Run on Windows
    Now you can !

--- meta/plugins/image.py
+++ meta/plugins/image.py
@@ -1,3 +1,4 @@
+import os
 from hashlib import blake2b
 from cutekit import model, builder, const, shell
 from pathlib import Path
@@ -23,21 +24,21 @@ class Image:
         return Path(builder.build(scope, comp)[0].path)
 
     def cp(self, src: Path, dst: Path) -> list[Path]:
-        shell.cp(str(src), str(self.path / str(dst).removeprefix("/")))
+        shell.cp(str(src), str(self.path / str(dst).removeprefix(os.path.sep)))
         return [dst]
 
     def mkdir(self, name: str):
         (self.path / str(name).removeprefix("/")).mkdir(parents=True, exist_ok=True)
 
     def wget(self, link: str, dst: str):
-        shell.wget(link, str(self.path / str(dst).removeprefix("/")))
+        shell.wget(link, str(self.path / str(dst).removeprefix(os.path.sep)))
 
     def get_hash(self, target: Path) -> str:
-        with (self.path / str(target).removeprefix("/")).open("rb") as f:
+        with (self.path / str(target).removeprefix(os.path.sep)).open("rb") as f:
             return blake2b(f.read()).hexdigest()
 
     def export_limine(self, target: Path, kernel: Path, modules: list[Path]):
-        rel_path = Path(self.path / str(target).removeprefix("/"))
+        rel_path = Path(self.path / str(target).removeprefix(os.path.sep))
         with (rel_path / "limine.conf").open("w") as f:
             f.writelines(
                 [
@@ -45,10 +46,10 @@ class Image:
                     "timeout: 0\n\n",
                     f"/{self.__name}\n",
                     "    protocol: limine\n",
-                    f"    kernel_path: boot():{kernel}#{self.get_hash(kernel)}\n",
+                    f"    kernel_path: boot():{kernel.as_posix()}#{self.get_hash(kernel)}\n",
                 ]
                 + [
-                    f"    module_path: boot():{module}#{self.get_hash(module)}\n"
+                    f"    module_path: boot():{module.as_posix()}#{self.get_hash(module)}\n"
                     for module in modules
                 ]
             )
